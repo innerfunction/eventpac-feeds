813be19d0040fb6d09e8636d1637e4a8acb19367
Marc-Antoine Pelletier <marco.pelletier@innerfunction.com>
2014-11-06 15:13:56 +0000

    update build script

--- aoife/index.js
+++ aoife/index.js
@@ -35,10 +35,28 @@ exports.build = function( cx ) {
 
     var types = ['events', 'speakers'];
 
-    var postsByType = types.reduce(function( posts, type ) {
-        posts[type] = cx.data.posts.filter(function(post) {
-            return post.type == type;
-        });
-    });
+	var types = ['news', 'events', 'resultsIndividual', 'resultsTeam'];
+	
+	var postsByType = types.reduce(function( posts, type ) {
+		posts[type] = cx.data.posts.filter(function( post ) {
+			return post.type == type;
+		});
+
+		var imageURLs = posts[type].map(function(post) {
+			return post.image;	
+		})
+		.filter(function( url ) {
+			return !!url;
+		});
+        var images = cx.images( imageURLs );
+        images.resize( { width: 100, format: 'jpeg' }, '{name}-{width}.{format}' ).mapTo( posts[type], 'thumbnail' );
+		images.resize( { width: 500, format: 'jpeg' }, true ).mapTo( posts[type], 'image' );
+		return posts;
+	}, {});
+
+    var eventFiles = cx.eval('template/events-details.html', postsByType.events, 'events-{id}.html');
+    cx.eval('template/speakers-details.html', postsByType.speakers, 'speakers-{id}.html');
+
+
 }
 exports.inpath = require('path').dirname(module.filename);
