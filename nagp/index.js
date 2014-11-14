9a09c2ad38a09264a6ac42daa5d04ed3f4e5a29a
Marc-Antoine Pelletier <marco.pelletier@innerfunction.com>
2014-11-14 12:38:51 +0000

    fix time date format nagp

--- nagp/index.js
+++ nagp/index.js
@@ -29,9 +29,9 @@ exports.download = function( cx ) {
             title:          post.title,
             occurrences:    post.occurrences,
             startDate:      mods.df(post.occurrences[0].startDateTime, 'dddd, mmmm dS'), //h:MM TT, mmmm dS, yyyy
-            startTime:      mods.df(post.occurrences[0].startDateTime, 'h:MM'),
+            startTime:      mods.df(post.occurrences[0].startDateTime, 'HH:MM'),
             endDate:        mods.df(post.occurrences[0].endDateTime, 'dddd, mmmm dS'),
-            endTime:        mods.df(post.occurrences[0].endDateTime, 'h:MM'),
+            endTime:        mods.df(post.occurrences[0].endDateTime, 'HH:MM'),
             content:        post.content,
             performer:      post.performers,
             type:           post.postType
