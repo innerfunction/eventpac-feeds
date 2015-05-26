25014abf7e2234aa395e5557afc52ea9e76a59a6
Julian Goacher <julian.goacher@innerfunction.com>
2015-05-26 12:35:31 +0100

    Removed obsolete feeds.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 exports.feeds = {
     'onf1': require('./onf1'),
-    'aoife': require('./aoife'),
-    'nagp': require('./nagp'),
-    'ecommerceexpo': require('./ecommerceexpo'),
     'nslc2015': require('./nslc2015')
 }
